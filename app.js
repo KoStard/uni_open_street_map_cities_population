ffe53a96fb240a54002416a695c9627fe4af6b84
KoStard <rub099999720@gmail.com>
2023-01-13 22:24:35 +0100

    Country detection and popup

--- app.js
+++ app.js
@@ -4,4 +4,11 @@ const app = express();
 const port = 3000;
 
 app.use(express.static('public'));
+
+// Listen to the get requests at /cities
+app.get('/cities', (req, res) => {
+    // return empty list
+    res.send([]);
+});
+
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- public/main.js
+++ public/main.js
@@ -1,18 +1,66 @@
-import {Map, View} from 'ol';
+import { Map, Overlay, View } from 'ol';
 import TileLayer from 'ol/layer/Tile';
+import VectorTileLayer from 'ol/layer/VectorTile';
 import XYZ from 'ol/source/XYZ';
+import VectorTileSource from 'ol/source/VectorTile';
+import MVT from 'ol/format/MVT';
+import { fromLonLat } from 'ol/proj';
 
-new Map({
+const map = new Map({
   target: 'map',
-  layers: [
-    new TileLayer({
-      source: new XYZ({
-        url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png'
-      })
-    })
-  ],
   view: new View({
-    center: [0, 0],
+    center: fromLonLat([0, 0]),
     zoom: 2
   })
+});
+
+// This one helps extracting the country name from the click
+const vectorTileLayer = new VectorTileLayer({
+  source: new VectorTileSource({
+    format: new MVT(),
+    url:
+      'https://ahocevar.com/geoserver/gwc/service/tms/1.0.0/' +
+      'ne:ne_10m_admin_0_countries@EPSG%3A900913@pbf/{z}/{x}/{-y}.pbf',
+    maxZoom: 14,
+  }),
+});
+map.addLayer(vectorTileLayer);
+
+const openStreetMapStandard = new TileLayer({
+  source: new XYZ({
+    url: 'https://{a-c}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+    maxZoom: 14,
+  })
+});
+map.addLayer(openStreetMapStandard);
+
+const container = document.getElementById('popup');
+const content = document.getElementById('popup-content');
+const popupCloser = document.getElementById('popup-closer');
+
+const overlay = new Overlay({
+  element: container,
+  autoPan: true,
+  autoPanAnimation: {
+    duration: 250
+  }
+});
+map.addOverlay(overlay);
+popupCloser.addEventListener('click', function () {
+  overlay.setPosition(undefined);
+  return false;
+});
+
+// Handle map clicks
+map.addEventListener('singleclick', function (evt) {
+  // Get the country using the coordinates from the map
+  const features = map.getFeaturesAtPixel(evt.pixel);
+  if (features.length === 0) {
+    overlay.setPosition(undefined);
+  } else {
+    const country = features[0].get('name');
+    console.log(features[0]); // TODO remove
+    // content.innerHTML = 'You clicked here: <code>' + country + '</code>';
+    overlay.setPosition(evt.coordinate);
+  }
 });
